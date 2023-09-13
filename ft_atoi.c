224bab04ba0e039e05b37b1faf30bf6b9e6650cd
Angel Mateo Rodriguez <anmateo-@c3r13s3.42madrid.com>
2023-09-13 10:39:13 +0200

    added functions ft_atoi, ft_strlcpy, ft_strlen, ft_strncmp

---
+++ ft_atoi.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anmateo- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/22 18:39:40 by anmateo-          #+#    #+#             */
+/*   Updated: 2023/06/22 18:39:41 by anmateo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	do_sign(char *str, int *ptr_i)
+{
+	int	count;
+	int	i;
+
+	i = 0;
+	count = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	while (str[i] && (str[i] == 43 || str[i] == 45))
+	{
+		if (str[i] == 45)
+			count *= -1;
+		i++;
+	}
+	*ptr_i = i;
+	return (count);
+}
+
+int	ft_atoi(char *str)
+{
+	int	sign;
+	int	result;
+	int	i;
+
+	result = 0;
+	sign = do_sign(str, &i);
+	while (str[i] && str[i] >= 48 && str[i] <= 57)
+	{
+		result *= 10;
+		result += str[i] - 48;
+		i++;
+	}
+	result *= sign;
+	return (result);
+}
