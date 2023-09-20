c75e72771ab23465ce8e6f82d53cc4d2c22d5ed7
Angel Mateo Rodriguez <anmateo-@c3r11s2.42madrid.com>
2023-09-20 11:01:02 +0200

    added ft_memset and fixed memcpy and memset

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,28 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anmateo- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/20 10:16:24 by anmateo-          #+#    #+#             */
-/*   Updated: 2023/09/20 11:00:21 by anmateo-         ###   ########.fr       */
+/*   Created: 2023/09/20 10:40:35 by anmateo-          #+#    #+#             */
+/*   Updated: 2023/09/20 10:59:53 by anmateo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "libft.h"
 
-void	*ft_memset(void *s, int c, size_t n)
+void	ft_memmove(void *dest, const void *src, size_t n)
 {
-	usigned long	i;
-	char			*d;
+	unsigned int	i;
 
-	d = (char *)s;
-	i = 0;
-	while (i < n)
+	if (dest != 0 && src != 0)
 	{
-		d[i] = char (c);
-		i++;
+		if (dest < src)
+			ft_memcpy(dest, src, n);
+		else
+		{
+			i = 0;
+			while (i < n)
+			{
+				*(unsigned char *)(dest + i) = *(unsigned char *)(src + i);
+				i++;
+			}
+		}
 	}
-	return (s);
+	return (dest);
 }
