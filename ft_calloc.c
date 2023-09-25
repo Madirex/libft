912d7ff8541c60c090d2eee0f45ab254e5940a08
Angel Mateo Rodriguez <anmateo-@c3r11s2.42madrid.com>
2023-09-25 10:15:39 +0200

    fixed calloc, itoa and memset

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anmateo- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/25 10:15:04 by anmateo-          #+#    #+#             */
+/*   Updated: 2023/09/25 10:15:15 by anmateo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void    *ft_calloc(size_t count, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
-    void    *mem;
+	void	*mem;
 
-    mem = (void*)malloc(count * size);
-    if (mem != 0)
-        ft_bzero(mem, count);
-    return (mem);
+	mem = (void *)malloc(count * size);
+	if (mem != 0)
+		ft_bzero(mem, count);
+	return (mem);
 }
--- ft_itoa.c
+++ ft_itoa.c
@@ -1,46 +1,58 @@
-char    *ft_itoa(int nb)
-{
-    char    *str;
-    long    nbl;
-    int i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anmateo- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/25 10:14:35 by anmateo-          #+#    #+#             */
+/*   Updated: 2023/09/25 10:14:55 by anmateo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    nbl = nb;
-    i = ft_numlen(nbl);
-    str = (char *)malloc(sizeof(char) * (i + 1));
-    if (!str)
-        return (0);
-    str[i--] = '\0';
-    if (nbl == 0)
-    {
-        str[0] = '0';
-    }
-    else if (nbl < 0)
-    {
-        str[0] = '-';
-        nbl = nbl * -1;
-    }
-    while (nbl > 0)
-    {
-        str[i--] = '0' + (nbl % 10);
-        nbl = nbl / 10;
-    }
-    return (str);
+char	*ft_itoa(int nb)
+{
+	char	*str;
+	long	nbl;
+	int		i;
+
+	nbl = nb;
+	i = ft_numlen(nbl);
+	str = (char *)malloc(sizeof(char) * (i + 1));
+	if (!str)
+		return (0);
+	str[i--] = '\0';
+	if (nbl == 0)
+	{
+		str[0] = '0';
+	}
+	else if (nbl < 0)
+	{
+		str[0] = '-';
+		nbl = nbl * -1;
+	}
+	while (nbl > 0)
+	{
+		str[i--] = '0' + (nbl % 10);
+		nbl = nbl / 10;
+	}
+	return (str);
 }
 
-int ft_numlen(long n)
+int	ft_numlen(long n)
 {
-    int len;
+	int	len;
 
-    len = 0;
-    if (n < 0)
-    {
-        n *= -1;
-        len++;
-    }
-    while (n > 0)
-    {
-        n = n / 10;
-        len++;
-    }
-    retrun (len);
+	len = 0;
+	if (n < 0)
+	{
+		n *= -1;
+		len++;
+	}
+	while (n > 0)
+	{
+		n = n / 10;
+		len++;
+	}
+	retrun(len);
 }
