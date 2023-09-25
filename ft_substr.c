a9e22307c01e5272ab4fbe9c9035846886c59651
Angel Mateo Rodriguez <anmateo-@c3r11s2.42madrid.com>
2023-09-25 12:33:32 +0200

    added ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: anmateo- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 10:58:23 by anmateo-          #+#    #+#             */
-/*   Updated: 2023/09/25 11:08:46 by anmateo-         ###   ########.fr       */
+/*   Updated: 2023/09/25 12:25:09 by anmateo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*str_return;
-	size_t	len_return;
 	unsigned int	i;
+	char			*str_return;
+	size_t			len_return;
 
 	len_return = 0;
 	if (s)
 		len_return = ft_strlen(s) - (len - start);
-	str_return = malloc((len_return + 1) * size_of(char));
+	str_return = malloc((len_return + 1) * sizeof(char));
 	if (!str_return)
 		return (0);
 	else
