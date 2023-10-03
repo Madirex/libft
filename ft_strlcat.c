ef611f22a5e9029f97a98c125c067fc358d1e7aa
Madirex <madirexland@gmail.com>
2023-10-03 11:34:52 +0200

    fixes

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: anmateo- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 11:14:33 by anmateo-          #+#    #+#             */
-/*   Updated: 2023/10/03 11:33:39 by anmateo-         ###   ########.fr       */
+/*   Updated: 2023/10/03 11:34:44 by anmateo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,9 @@ size_t	ft_strlcat(char *dest, const char *src, size_t n)
 
 	dest_len = ft_strlen(dest);
 	src_len = ft_strlen((char *)src);
-
 	if (n <= dest_len)
 		return (n + src_len);
 	i = 0;
-
 	while (src[i] && (dest_len + i) < (n - 1))
 	{
 		dest[dest_len + i] = src[i];
