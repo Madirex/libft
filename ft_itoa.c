858182f2617eeb22616639ab28fec1c954e15f67
Madirex <madirexland@gmail.com>
2023-10-06 11:23:12 +0200

    fixed ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,25 +6,24 @@
 /*   By: anmateo- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 10:14:35 by anmateo-          #+#    #+#             */
-/*   Updated: 2023/10/04 10:25:40 by anmateo-         ###   ########.fr       */
+/*   Updated: 2023/10/06 11:21:28 by anmateo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static long	ft_numlen(long n)
+static int	ft_numlen(int n)
 {
-	long	len;
+	int	len;
 
 	len = 0;
+	if (n == 0)
+		return (1);
 	if (n < 0)
-	{
 		len++;
-		n *= -1;
-	}
-	while (n > 0)
+	while (n)
 	{
-		n = n / 10;
+		n /= 10;
 		len++;
 	}
 	return (len);
@@ -33,28 +32,27 @@ static long	ft_numlen(long n)
 char	*ft_itoa(int nb)
 {
 	char	*str;
-	long	nbl;
-	long	i;
+	int		nbl;
+	int		i;
 
+	if (nb == -2147483648)
+		return (ft_strdup("-2147483648"));
 	nbl = nb;
-	i = ft_numlen(nbl);
-	str = (char *)malloc(sizeof(char) * (i + 1));
+	if (nb < 0)
+		nbl = -nb;
+	i = ft_numlen(nb);
+	str = malloc(sizeof(char) * (i + 1));
 	if (!str)
 		return (0);
 	str[i--] = '\0';
 	if (nbl == 0)
-	{
-		str[0] = '0';
-	}
-	else if (nbl < 0)
-	{
-		str[0] = '-';
-		nbl = nbl * -1;
-	}
+		str[i] = '0';
 	while (nbl > 0)
 	{
-		str[i--] = '0' + (nbl % 10);
-		nbl = nbl / 10;
+		str[i--] = (nbl % 10) + '0';
+		nbl /= 10;
 	}
+	if (nb < 0)
+		str[i] = '-';
 	return (str);
 }
