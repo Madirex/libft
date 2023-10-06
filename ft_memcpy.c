c0a8c8a56d025bab3533bfdadfac9db4d6643293
Madirex <madirexland@gmail.com>
2023-10-06 12:21:54 +0200

    fixed memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,21 +6,26 @@
 /*   By: anmateo- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 10:27:30 by anmateo-          #+#    #+#             */
-/*   Updated: 2023/10/04 12:16:50 by anmateo-         ###   ########.fr       */
+/*   Updated: 2023/10/06 12:21:12 by anmateo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	unsigned char	*d;
-	unsigned char	*s;
+	unsigned const char	*a;
+	unsigned char		*b;
+	size_t				i;
 
-	if (!dst && !src)
-		return (0);
-	d = (unsigned char *)dst;
-	s = (unsigned char *)src;
-	while (n--)
-		*d++ = *s++;
+	a = (unsigned char *)src;
+	b = (unsigned char *)dst;
+	i = 0;
+	while (!n || dst == src)
+		return (dst);
+	while (n > i)
+	{
+		b[i] = a[i];
+		i++;
+	}
 	return (dst);
 }
