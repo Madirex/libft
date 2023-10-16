a7d6cc263ee2dbe53d4f6cd1e06ea7a3f8624a87
Madirex <madirexland@gmail.com>
2023-10-16 12:51:49 +0200

    changes calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: anmateo- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 10:15:04 by anmateo-          #+#    #+#             */
-/*   Updated: 2023/10/11 12:25:33 by anmateo-         ###   ########.fr       */
+/*   Updated: 2023/10/16 12:47:54 by anmateo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
-	if ((size * count) > SIZE_MAX)
-		return (NULL);
 	ptr = malloc(size * count);
 	if (!ptr)
 		return (0);
